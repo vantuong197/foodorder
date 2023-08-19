1ddfe2adc2e0215677a542335f91404ddb6054c0
vantuong <vantuong197@gmail.com>
2023-08-19 09:52:49 +0900

    add login file

---
+++ backend/login.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Login Page - Food Order System</title>
+        <link rel="stylesheet" href="../frontend/css/backend.css">
+    </head>
+    <body>
+        
+        <div class="login">
+            <h1>Login </h1>
+            <p>Created By - <a href="">Tuong Tran</a></p>
+        </div>
+    </body>
+</html>
